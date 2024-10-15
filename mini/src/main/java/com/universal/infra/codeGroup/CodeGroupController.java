bda66266bb619531e9d9c7fd58ebf6f346d18dc2
imdoyoung <bear2008@naver.com>
2024-10-15 19:06:15 +0900

    CODEGROUP-codeGroupXdmForm.html 생성

--- mini/src/main/java/com/universal/infra/codeGroup/CodeGroupController.java
+++ mini/src/main/java/com/universal/infra/codeGroup/CodeGroupController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CodeGroupController {
 	
+	
+	// List
 	@RequestMapping(value="/xdm/v1/infra/codeGroup/codeGroupXdmList")
 	public String codeGroupXdmList() {
 		return "/xdm/v1/infra/codeGroup/codeGroupXdmList";
+	}
+	
+	// Form
+	@RequestMapping(value="/xdm/v1/infra/codeGroup/codeGroupXdmForm")
+	public String codeGroupXdmForm() {
+		return "/xdm/v1/infra/codeGroup/codeGroupXdmForm";
 	}
 
 }
